7b22726271e9b116e1d0e964354f28799f99c4f1
Sisir K. Adhikari <sisir@tutremix.com>
2018-10-03 01:25:31 +0600

    WP minimum version bump

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/includes/includes.php';
 
 // define constants
 define('CM_PHP_VERSION', '5.4');
-define('CM_WP_VERSION', '3.5');
+define('CM_WP_VERSION', '4.0');
 define('CM_TEXT_DOMAIN', 'codeline-movies');
 
 // singleton 
